24ca36e067678fcab0677969eda513d0f071dad3
LightmareXO <175393228+LightmareXO@users.noreply.github.com>
2026-05-04 01:07:16 +0900

    Set up routing and move home content to Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,17 @@
-import { useState } from 'react'
 import './App.css'
-import TaskList from './components/TaskList/TaskList';
-import { uuidv7 } from 'uuidv7';
-import Sidebar from './components/Sidebar/Sidebar';
-import AddTaskButton from './components/AddTask/AddTaskButton';
-import AddTaskModal from './components/AddTask/AddTaskModal';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
 
 
 function App() {
-	const [tasks, setTasks] = useState([
-		{ id: uuidv7(), name: 'test task', dueDate: '2026-06-01', dueTime: '12:00', completed: false },
-	])
-	const [isOpenModal, setIsOpenModal] = useState(false)
-
-	const addTask = (taskName, taskDueDate, taskDueTime) => {
-		if (taskName.trim() === '') return
-		setTasks([...tasks, { id: uuidv7(), name: taskName, dueDate: taskDueDate, dueTime: taskDueTime, completed: false }])
-	}
-
-	const deleteTask = (id) => {
-		setTasks(tasks.filter((task) => task.id !== id))
-	}
-	const toggleTask = (id) => {
-		setTasks(tasks.map((task) => task.id === id ? { ...task, completed: !task.completed } : task))
-	}
-
-	const openModal = () => setIsOpenModal(true)
-	const closeModal = () => setIsOpenModal(false)
-
 	return (
-		<div className="flex min-h-screen">
-			<Sidebar/>
-			<div className='flex-1 p-4'>
-				
-				<div>
-					remain: {tasks.filter((task) => !task.completed).length}
-					<TaskList tasks={tasks.filter((task) => !task.completed)} deleteTask={deleteTask} toggleTask={toggleTask}/>
-				</div>
-
-				<div className='my-4'>
-					completed
-					<TaskList tasks={tasks.filter((task) => task.completed)} deleteTask={deleteTask} toggleTask={toggleTask}/>
-				</div>
-				
-
-				<AddTaskButton onOpen={openModal}/>
-				<AddTaskModal onAddTask={addTask} isOpenModal={isOpenModal} onCloseModal={closeModal}/>
-			</div>
-		</div>
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />}/>
+				</Routes>
+			</BrowserRouter>
+		</>
 	)
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,62 @@
+import Sidebar from "../components/Sidebar/Sidebar";
+import TaskList from "../components/TaskList/TaskList";
+import AddTaskButton from "../components/AddTask/AddTaskButton";
+import AddTaskModal from "../components/AddTask/AddTaskModal";
+import { uuidv7 } from 'uuidv7';
+import { useState } from "react";
+
+
+function Home() {
+	const [tasks, setTasks] = useState([
+		{ id: uuidv7(), name: 'test task', dueDate: '2026-06-01', dueTime: '12:00', completed: false },
+	])
+	const [isOpenModal, setIsOpenModal] = useState(false)
+
+	const addTask = (taskName, taskDueDate, taskDueTime) => {
+		if (taskName.trim() === '') return
+		setTasks([...tasks, { id: uuidv7(), name: taskName, dueDate: taskDueDate, dueTime: taskDueTime, completed: false }])
+	}
+
+	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id))
+	}
+	const toggleTask = (id) => {
+		setTasks(tasks.map((task) => task.id === id ? { ...task, completed: !task.completed } : task))
+	}
+
+	const openModal = () => setIsOpenModal(true)
+	const closeModal = () => setIsOpenModal(false)
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 p-4">
+        <div>
+          remain: {tasks.filter((task) => !task.completed).length}
+          <TaskList
+            tasks={tasks.filter((task) => !task.completed)}
+            deleteTask={deleteTask}
+            toggleTask={toggleTask}
+          />
+        </div>
+
+        <div className="my-4">
+          completed
+          <TaskList
+            tasks={tasks.filter((task) => task.completed)}
+            deleteTask={deleteTask}
+            toggleTask={toggleTask}
+          />
+        </div>
+
+        <AddTaskButton onOpen={openModal} />
+        <AddTaskModal
+          onAddTask={addTask}
+          isOpenModal={isOpenModal}
+          onCloseModal={closeModal}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
