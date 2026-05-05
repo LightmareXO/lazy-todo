1ac9833155ecb70113fe17f7b4d60df7dd027c47
LightmareXO <175393228+LightmareXO@users.noreply.github.com>
2026-05-05 21:04:27 +0900

    set header in settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -12,7 +12,8 @@ function Settings() {
 
   return (
     <>
-      <button className="border p-2 rounded-lg text-red-500 hover:bg-red-100" onClick={handleDeleteData}>
+      <h1 className="text-3xl pb-2 border-b border-gray-400">Settings</h1>
+      <button className="border my-4 p-2 rounded-lg text-red-500 hover:bg-red-100" onClick={handleDeleteData}>
         Delete All Data
       </button>
     </>
