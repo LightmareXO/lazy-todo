f13cb3641be0bd3dccc255e003af242c1b930248
LightmareXO <175393228+LightmareXO@users.noreply.github.com>
2026-05-05 20:11:15 +0900

    Add Delete Data Button

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,21 @@
 
 function Settings() {
+
+  const handleDeleteData = () => {
+    const shouldDelete = window.confirm(`全データを削除しますか？`)
+
+    if (!shouldDelete) return
+    
+    localStorage.clear()
+    window.location.reload()
+  }
+
   return (
-    <div>Settings</div>
+    <>
+      <button className="border p-2 rounded-lg text-red-500 hover:bg-red-100" onClick={handleDeleteData}>
+        Delete All Data
+      </button>
+    </>
   )
 }
 
