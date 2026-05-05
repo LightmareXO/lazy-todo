cf66e895078164f1fc289aede5f27bd22e00735c
LightmareXO <175393228+LightmareXO@users.noreply.github.com>
2026-05-05 22:19:49 +0900

    feat: show empty task message on task pages

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,9 +1,12 @@
 import Task from './Task'
 
 const TaskList = ({ tasks, deleteTask, toggleTask, setInEditing }) => {
+
+	if (tasks.length === 0) return <p className="text-center text-2xl p-2">No tasks here!</p>
+
   return (
 	<>
-      {tasks.map((task) => <Task key={task.id} task={task} deleteTask={deleteTask} toggleTask={toggleTask} setInEditing={setInEditing}/>)}
+    {tasks.map((task) => <Task key={task.id} task={task} deleteTask={deleteTask} toggleTask={toggleTask} setInEditing={setInEditing}/>)}
 	</>
 	)
 }
