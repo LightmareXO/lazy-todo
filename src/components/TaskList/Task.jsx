21f92a68361676020e6f0f707f51c637ce261f86
LightmareXO <175393228+LightmareXO@users.noreply.github.com>
2026-05-05 22:42:38 +0900

    fix: add word truncate for complete and incomplete task names

--- src/components/TaskList/Task.jsx
+++ src/components/TaskList/Task.jsx
@@ -30,16 +30,29 @@ const Task = ({ task, deleteTask, toggleTask, setInEditing }) => {
   return (
     <>
       <div className="p-2 my-4 border border-gray-400 rounded-lg">
-        <div className="flex items-center rounded-lg h-10">
+        <div
+          className={`relative grid items-center gap-3 rounded-lg ${
+            task.completed
+              ? "grid-cols-[minmax(0,7fr)_250px_32px]"
+              : "grid-cols-[10px_minmax(0,6fr)_250px_32px]"
+          }`}
+        >
+          
           {!task.completed && 
-            <button className="bg-transparent text-gray-200 font-bold hover:text-gray-700 h-6 w-6 text-sm pt-0.5 pr-0.5 border border-gray-600 rounded-full hover:cursor-pointer" onClick={() => toggleTask(task.id)}>
+            <button className="items-center bg-transparent text-gray-200 font-bold hover:text-gray-700 h-6 w-6 text-sm pt-0.5 pr-0.5 border border-gray-600 rounded-full hover:cursor-pointer" onClick={() => toggleTask(task.id)}>
             ✔
             </button>
           }
-          <div className="ml-3 flex-1 text-lg font-bold text-gray-800">{task.name}</div>
-          <div className="justify-end text-gray-500">Due:</div>
-          <div className="ml-5 justify-end text-gray-500">{task.dueDate}</div>
-          <div className="flex-1 ml-5 justify-end text-gray-500">{task.dueTime}</div>
+
+          <div className="px-5 min-w-0 text-lg font-bold text-gray-800 truncate"> 
+            {task.name}
+          </div>
+          
+          <div>
+            <span className=" text-gray-500">Due:</span>
+            <span className="ml-5 text-gray-500">{task.dueDate}</span>
+            <span className="ml-5 text-gray-500">{task.dueTime}</span>
+          </div>
           
           <div ref={menuRef} className="relative ml-auto hover:cursor-pointer pr-4">
             <span onClick={() => setIsMenuOpen(prev => !prev)}>
