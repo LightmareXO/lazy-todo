140adbdfe41bfbc8859e68a358a9b030217e847e
LightmareXO <175393228+LightmareXO@users.noreply.github.com>
2026-05-07 02:40:21 +0900

    feat: add edit a task button in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import AddTaskButton from "../components/AddTask/AddTaskButton";
 import AddTaskModal from "../components/AddTask/AddTaskModal";
+import EditTaskModal from "../components/EditTask/EditTaskModal";
 import SortButton from "../components/SortButton";
 import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
@@ -8,12 +9,14 @@ function Home() {
   const {
     addTask,
     deleteTask,
+    editTask,
     toggleTask,
     sortedTasks,
     sortMode,
     toggleSortMode,
   } = useOutletContext();
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const [inEditing, setInEditing] = useState(null);
 
   const openModal = () => setIsOpenModal(true);
   const closeModal = () => setIsOpenModal(false);
@@ -53,16 +56,22 @@ function Home() {
               <div className="flex h-full mr-5 items-center justify-end">
                 <button
                   onClick={() => toggleTask(nextTask.id)}
-                  className="shrink-0 rounded-full border border-gray-400 px-5 py-3 hover:bg-gray-200 active:bg-gray-300"
+                  className="shrink-0 rounded-full border border-gray-400 px-5 py-3 hover:bg-gray-200 active:bg-gray-300 hover:cursor-pointer"
                 >
                   Done!
                 </button>
               </div>
               <button 
-                className="absolute top-2 right-2 hover:bg-gray-200 active:bg-gray-300 h-7 w-7 rounded-full hover:cursor-pointer" 
+                className="absolute bottom-2 right-2 hover:bg-gray-200 active:bg-gray-300 h-7 w-7 rounded-full hover:cursor-pointer" 
                 onClick={() => handleDelete(nextTask)}
               >
                 <i className="ri-delete-bin-line"></i>
+              </button>
+              <button 
+                className="absolute bottom-2 right-10 hover:bg-gray-200 active:bg-gray-300 h-7 w-7 rounded-full hover:cursor-pointer" 
+                onClick={() => setInEditing(nextTask)}
+              >
+                <i className="ri-edit-line"></i>
               </button>
             </div>
           </div>
@@ -87,6 +96,11 @@ function Home() {
         onAddTask={addTask}
         isOpenModal={isOpenModal}
         onCloseModal={closeModal}
+      />
+      <EditTaskModal
+        inEditing={inEditing}
+        editTask={editTask}
+        setInEditing={setInEditing}
       />
     </div>
   );
